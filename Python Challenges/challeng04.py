51e655335353681bcbc2944add78ad89bd8f2749
alisalimi77 <ali.salimi77@gmail.com>
2023-05-25 13:11:32 +0330

    change the challen04.py

--- Python Challenges/challeng04.py
+++ Python Challenges/challeng04.py
@@ -1,4 +1,4 @@
-Palindrome: Write a program that checks whether a given string is a palindrome or not.
+# Palindrome: Write a program that checks whether a given string is a palindrome or not.
 
 def is_palindrome(string):
     # Remove whitespace and convert string to lowercase
