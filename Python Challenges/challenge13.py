d5990d777d1b83b1b3d19166fc02b80ab69b9195
alisalimi77 <ali.salimi77@gmail.com>
2023-05-25 13:57:45 +0330

    change the challenge13.py

--- Python Challenges/challenge13.py
+++ Python Challenges/challenge13.py
@@ -1,2 +1,21 @@
-Count the Number of Words: Write a program that counts the number of words in a string.
+#Count the Number of Words: Write a program that counts the number of words in a string.
+def count_words(string):
+    # Remove leading and trailing whitespace
+    string = string.strip()
+    # Split the string into words
+    words = string.split()
+    # Return the count of words
+    return len(words)
 
+# User interface & Test the function
+print("Word Count Program")
+print("------------------")
+while True:
+    input_string = input("Enter a string (or 'q' to quit): ")
+    if input_string.lower() == 'q':
+        break
+    word_count = count_words(input_string)
+    print("The number of words in the string is:", word_count)
+    print()
+
+print("Thank you for using the Word Count Program. Goodbye!")
