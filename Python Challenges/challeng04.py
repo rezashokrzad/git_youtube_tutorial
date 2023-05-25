83f969d249f05e5da7f0d0ce6d9554fd9bed1ad5
alisalimi77 <ali.salimi77@gmail.com>
2023-05-25 13:07:07 +0330

    Update challeng04.py
    challenge 4

--- Python Challenges/challeng04.py
+++ Python Challenges/challeng04.py
@@ -1,2 +1,14 @@
 Palindrome: Write a program that checks whether a given string is a palindrome or not.
 
+def is_palindrome(string):
+    # Remove whitespace and convert string to lowercase
+    string = string.lower().replace(" ", "")
+    # Check if the string is equal to its reverse
+    return string == string[::-1]
+
+# Test the function
+input_string = input("Enter a string: ")
+if is_palindrome(input_string):
+    print("The string is a palindrome.")
+else:
+    print("The string is not a palindrome.")
