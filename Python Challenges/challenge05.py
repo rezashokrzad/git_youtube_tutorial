576c216ee572eaddc673351bc6a3c0e679ce0000
Hassan Akhavanpour <72379329+hakhavanpou82@users.noreply.github.com>
2023-05-31 15:48:25 +0200

    Update challenge05.py
    For reversing any string, a python function was defined.

--- Python Challenges/challenge05.py
+++ Python Challenges/challenge05.py
@@ -1,2 +1,10 @@
 Reverse String: Write a program that takes a string as input and outputs the string reversed.
 
+def reverse_string(input_string):
+  reversed_str = input_string[::-1]
+  return reversed_str
+
+# Calling function
+user_input = input("Enter a favorite string: ")
+reversed_input = reverse_string(user_input)
+print("Reversed string:", reversed_input)
