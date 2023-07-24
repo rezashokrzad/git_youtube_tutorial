46a3305a3fc84311ba1a196dc231346db1827b58
hossein <hosseinzeinali1508@gmail.com>
2023-07-24 13:57:45 +0330

    update challenge18.py

--- Python Challenges/challenge18.py
+++ Python Challenges/challenge18.py
@@ -1,2 +1,10 @@
-Sum of Digits: Write a program that calculates the sum of the digits of a number entered by the user.
+#Sum of Digits: Write a program that calculates the sum of the digits of a number entered by the user.
 
+def sOD(number):
+    sum=0
+    for i in number:
+        sum += int(i)
+    return sum
+      
+number=input("enter number: ")
+print(sOD(number))
