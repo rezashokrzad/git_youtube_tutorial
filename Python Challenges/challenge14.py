4bc8687582fce91bec03fd6827ba79b286215d76
hossein <hosseinzeinali1508@gmail.com>
2023-07-24 17:28:10 +0330

    update python challenges

--- Python Challenges/challenge14.py
+++ Python Challenges/challenge14.py
@@ -1,2 +1,6 @@
-Count the Number of Characters: Write a program that counts the number of characters in a string.
-
+#Count the Number of Characters: Write a program that counts the number of characters in a string.
+def Count(text):
+    result=len([i for i in text if i.isalpha()])
+    return result
+string=input("enter string : ")
+print(Count(string))
--- Python Challenges/challenge16.py
+++ Python Challenges/challenge16.py
@@ -1,2 +1,12 @@
-Prime Numbers: Write a program that determines whether a given number is prime or not.
+#Prime Numbers: Write a program that determines whether a given number is prime or not.
 
+def isprime(num):
+    acc=0
+    for i in range(1,num+1):
+        if num%i==0:
+            acc+=1
+    return True if acc==2 else False
+
+num=int(input("enter a number: "))
+print(isprime(num))
+    
